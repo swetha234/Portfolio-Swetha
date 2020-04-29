cc720c0d9c66175915b0e76264feabb13895153e
swetha234 <hgutt001@odu.edu>
2020-04-29 15:16:46 -0400

    First commit

---
+++ src/components/aboutme.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        Results-oriented Web Developer with 2+ years of experience and expertise
+        in Web Development, Object-Oriented Concepts, Algorithms and Data
+        Structures. Dedicated to build, enhance, user-friendly, and feature-rich
+        websites. Willingness and ability to grasp concepts quickly and expand
+        the present knowledge base with ease.
+      </div>
+    );
+  }
+}
+
+export default About;
