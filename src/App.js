f56df7ba7ec6febf92515b098050dc385471fc86
swetha234 <hgutt001@odu.edu>
2020-05-07 19:47:57 -0400

    Making responsive

--- src/App.js
+++ src/App.js
@@ -8,110 +8,110 @@ import Main from './components/main';
 import { BrowserRouter as Link } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='demo-big-content'>
-        <Layout>
-          <Header
-            className='header-color'
-            title={
-              <a
-                style={{
-                  textDecoration: 'none',
-                  color: 'white',
-                  fontFamily: 'LTC Bodoni 175',
-                  fontSize: '17px'
-                }}
-                className='btn btn-5'
-                href='/'
-              >
-                MyPortfolio
-              </a>
-            }
-            scroll
-          >
-            <Navigation>
-              <a
-                href='/resume'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-                className='btn btn-5'
-              >
-                Resume
-              </a>
-              <a
-                href='/aboutme'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-                className='btn btn-5'
-              >
-                About Me
-              </a>
-              <a
-                href='/projects'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-                className='btn btn-5'
-              >
-                Projects
-              </a>
-              <a
-                href='/contact'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-                className='btn btn-5'
-              >
-                Contact
-              </a>
-            </Navigation>
-          </Header>
-          <Drawer
-            title={
-              <Link
-                style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                  fontFamily: 'LTC Bodoni 175',
-                  fontSize: '17px'
-                }}
-                href='/'
-              >
-                MyPortfolio
-              </Link>
-            }
-          >
-            <Navigation>
-              <a
-                href='resume'
-                class='btn btn-5'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-              >
-                Resume
-              </a>
-              <a
-                href='/aboutme'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-              >
-                About Me
-              </a>
-              <a
-                href='/projects'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-              >
-                Projects
-              </a>
-              <a
-                href='/contact'
-                style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
-              >
-                Contact
-              </a>
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className='page-content' />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='demo-big-content'>
+				<Layout>
+					<Header
+						className='header-color'
+						title={
+							<a
+								style={{
+									textDecoration: 'none',
+									color: 'white',
+									fontFamily: 'LTC Bodoni 175',
+									fontSize: '17px'
+								}}
+								className='btn btn-5'
+								href='/'
+							>
+								MyPortfolio
+							</a>
+						}
+						scroll
+					>
+						<Navigation>
+							<a
+								href='/resume'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+								className='btn btn-5'
+							>
+								Resume
+							</a>
+							<a
+								href='/aboutme'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+								className='btn btn-5'
+							>
+								About Me
+							</a>
+							<a
+								href='/projects'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+								className='btn btn-5'
+							>
+								Projects
+							</a>
+							<a
+								href='/contact'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+								className='btn btn-5'
+							>
+								Contact
+							</a>
+						</Navigation>
+					</Header>
+					<Drawer
+						title={
+							<a
+								style={{
+									textDecoration: 'none',
+									color: 'black',
+									fontFamily: 'LTC Bodoni 175',
+									fontSize: '17px'
+								}}
+								href='/'
+							>
+								MyPortfolio
+							</a>
+						}
+					>
+						<Navigation>
+							<a
+								href='resume'
+								class='btn btn-5'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+							>
+								Resume
+							</a>
+							<a
+								href='/aboutme'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+							>
+								About Me
+							</a>
+							<a
+								href='/projects'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+							>
+								Projects
+							</a>
+							<a
+								href='/contact'
+								style={{ fontFamily: 'LTC Bodoni 175', fontSize: '17px' }}
+							>
+								Contact
+							</a>
+						</Navigation>
+					</Drawer>
+					<Content>
+						<div className='page-content' />
+						<Main />
+					</Content>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,62 +4,62 @@ import { Grid, Cell } from 'react-mdl';
 // import SplitPane from 'react-split-pane';
 
 class About extends Component {
-  render() {
-    return (
-      <div style={{ width: '100%' }}>
-        <div className='left-half' style={{ float: 'left', width: '40%' }}>
-          <Grid
-            className='demo-grid-1'
-            style={{ paddingLeft: '10%', paddingTop: '10%' }}
-          >
-            <Cell col={6}>
-              <img src='images/dogmom.jpg' alt='Avatar' />
-            </Cell>
-            <Cell col={6}>
-              <img src='images/virgo.jpg' alt='Avatar' />
-            </Cell>
-          </Grid>
-          <Grid
-            className='demo-grid-2'
-            style={{ paddingLeft: '10%', paddingTop: '2%' }}
-          >
-            <Cell col={6}>
-              <img src='images/dance.jpeg' alt='Avatar' />
-            </Cell>
-            <Cell col={6}>
-              <img src='images/cooking.jpg' alt='Avatar' />
-            </Cell>
-          </Grid>
-          <Grid
-            className='demo-grid-3'
-            style={{ paddingLeft: '10%', paddingTop: '2%' }}
-          >
-            <Cell col={6}>
-              <img src='images/ocd.jpg' alt='Avatar' />
-            </Cell>
-            <Cell col={6}>
-              <img src='images/yoga.png' alt='Avatar' />
-            </Cell>
-          </Grid>
-        </div>
-        <div className='right-half' style={{}}>
-          <h3 style={{ fontFamily: 'LTC Bodoni 175' }}>
-            {' '}
-            Hello, I'm Swetha....!
-          </h3>
-          <br></br>
-          <p style={{ fontFamily: 'LTC Bodoni 175' }}>
-            I am a passionate User Experience and User Interface Designer. You
-            have an idea, you need someone to bring it to life or you already
-            have a product that needs to be refined to meet the demand of 2020
-            users! Now, this is where I come in! With the knowledge and
-            creativity I ganied till now, I can take your ideas and your
-            products to next level. Let's have a chat and discuss your idea!
-          </p>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{ width: '100%' }}>
+				<div className='left-half' style={{ float: 'left', width: '40%' }}>
+					<Grid
+						className='demo-grid-1'
+						style={{ paddingLeft: '10%', paddingTop: '10%' }}
+					>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/dogmom.jpg' alt='Avatar' />
+						</Cell>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/virgo.jpg' alt='Avatar' />
+						</Cell>
+					</Grid>
+					<Grid
+						className='demo-grid-2'
+						style={{ paddingLeft: '10%', paddingTop: '2%' }}
+					>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/dance.jpeg' alt='Avatar' />
+						</Cell>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/cooking.jpg' alt='Avatar' />
+						</Cell>
+					</Grid>
+					<Grid
+						className='demo-grid-3'
+						style={{ paddingLeft: '10%', paddingTop: '2%' }}
+					>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/ocd.jpg' alt='Avatar' />
+						</Cell>
+						<Cell col={6} tablet={8} phone={8}>
+							<img src='images/yoga.png' alt='Avatar' />
+						</Cell>
+					</Grid>
+				</div>
+				<div className='right-half' style={{}}>
+					<h3 style={{ fontFamily: 'LTC Bodoni 175' }}>
+						{' '}
+						Hello, I'm Swetha....!
+					</h3>
+					<br></br>
+					<p style={{ fontFamily: 'LTC Bodoni 175' }}>
+						I am a passionate User Experience and User Interface Designer. You
+						have an idea, you need someone to bring it to life or you already
+						have a product that needs to be refined to meet the demand of 2020
+						users! Now, this is where I come in! With the knowledge and
+						creativity I ganied till now, I can take your ideas and your
+						products to next level. Let's have a chat and discuss your idea!
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default About;
