dbc483c74a10bed21da4c1749e591888b8e424e1
swetha234 <hgutt001@odu.edu>
2020-05-07 19:06:35 -0400

    Live Demo Links

--- src/components/experience.js
+++ src/components/experience.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 class Experience extends Component {
-  render() {
-    return (
-      <Grid>
-        <Cell col={4}>
-          <p>
-            {this.props.monthStart} {this.props.startYear} -{' '}
-            {this.props.monthEnd} {this.props.endYear}
-          </p>
-        </Cell>
-        <Cell col={8}>
-          <h5 style={{ marginTop: '0px' }}>
-            Company Name: {this.props.companyName}
-          </h5>
-          <h5 style={{ marginTop: '0px' }}>{this.props.jobName}</h5>
-          <p>{this.props.jobDescription}</p>
-        </Cell>
-      </Grid>
-    );
-  }
+	render() {
+		return (
+			<Grid>
+				<Cell col={4}>
+					<p>
+						{this.props.monthStart} {this.props.startYear} -{' '}
+						{this.props.monthEnd} {this.props.endYear}
+					</p>
+				</Cell>
+				<Cell col={8}>
+					<h5 style={{ marginTop: '0px' }}>
+						Worked At: {this.props.companyName}
+					</h5>
+					<h5 style={{ marginTop: '0px' }}>{this.props.jobName}</h5>
+					<p>{this.props.jobDescription}</p>
+				</Cell>
+			</Grid>
+		);
+	}
 }
 
 export default Experience;
